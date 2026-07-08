f13adc38c130227896850c76636ba92c89ed8a0a
nabilrn <2211522018_nabil@student.unand.ac.id>
2026-07-08 15:01:48 +0700

    amba

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -436,7 +436,7 @@ const indexHTML = `<!doctype html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Dummy Todos</title>
+  <title>Dummy Todos Cikiwir</title>
   <style>
     :root {
       color-scheme: light;
